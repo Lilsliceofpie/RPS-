2e93ff1cc7a240f9925c28d9ecf90763e5cf4f9c
Lilsliceofpie <bar.tynan@gmail.com>
2022-08-16 14:36:14 +0000

    added event listener for buttons and computersTurn function

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,3 +11,26 @@ let player;
 let computer;
 let result;
 let score;
+
+controlBtns.forEach(button => button.addEventListener("click", () => {
+
+    player = button.textContent
+    computersTurn();
+}));
+
+function computersTurn(){
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+        case 1:
+            computer = "Rock";
+            break;
+        case 2:
+            computer = "Paper";
+            break;
+        case 3:
+            computer = "Scissors";
+            break;       
+    }
+}
