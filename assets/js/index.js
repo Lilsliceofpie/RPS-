590da54d90150549d2dafeab62eb3e6df6e787e8
Lilsliceofpie <bar.tynan@gmail.com>
2022-08-16 14:21:14 +0000

    added score div and initial JS variables

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+/**
+* Declare constants for DOM elements
+*/
+const playerDiv = document.querySelector("#playerDiv");
+const computerDiv = document.querySelector("#computerDiv");
+const resultDiv = document.querySelector("#resultDiv");
+const scoreDiv = document.querySelector("#scoreDiv")
+const controlBtns = document.querySelectorAll(".controlBtn");
+
+let player;
+let computer;
+let result;
+let score;
